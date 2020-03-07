4288b52c3356832da4853d72cd6ebfe718abfb10
lunatik752 <whitemaxim752@gmail.com>
2020-03-07 13:51:11 +0300

    added app layout

--- src/App.js
+++ src/App.js
@@ -3,24 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://cdn2.vectorstock.com/i/1000x1000/48/46/guitar-logo-vector-10784846.jpg' alt='logo'/>
+            </header>
+            <div className='personalInfo'>
+                <div className='avatar'>
+                    <img src='https://semantica.in/wp-content/uploads/2018/08/av-427845.png' alt='my photo'/>
+                </div>
+                <div className='aboutMySelf'>
+                    personal information
+                </div>
+            </div>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    My posts
+                    <div>
+                        New posts
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                        <div>
+                            post 3
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;

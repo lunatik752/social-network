36fc28c67e08c2193a37ed9bb65d2cff4aa2b399
lunatik752 <whitemaxim752@gmail.com>
2020-04-26 21:12:22 +0300

    completed lesson 47 fixed adding messages and posts

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -5,36 +5,6 @@ import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
 
-
-// const MyPostsContainer = () => {
-//
-//     return (
-//         <StoreContext.Consumer>
-//             {
-//                 (store) => {
-//
-//                     let state = store.getState();
-//
-//                     let addPost = () => {
-//                         store.dispatch(addPostActionCreator());
-//                     };
-//
-//                     let onPostChange = (text) => {
-//                         let action = updateNewPostTextActionCreator(text);
-//                         store.dispatch(action);
-//                     };
-//
-//                     return <MyPosts updateNewPostText={onPostChange}
-//                                     addPost={addPost}
-//                                     posts={state.profilePage.posts}
-//                                     newPostText={state.profilePage.newPostText}
-//                     />
-//                 }
-//             }
-//         </StoreContext.Consumer>
-//     )
-// };
-
 const mapStateToProps = (state) => {
     return {
         newPostText: state.profilePage.newPostText,

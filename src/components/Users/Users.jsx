d1b36b8af19072a66e083f5b37872ba645ef914d
lunatik752 <whitemaxim752@gmail.com>
2020-05-05 11:41:45 +0300

    added axios and get request to server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,69 +1,82 @@
 import React from 'react';
 import style from './Usere.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/image/user.png'
+
 
 const Users = (props) => {
-if (props.users.length === 0) {
-    props.setUsers(
-        [
-            {
-                id: 1,
-                photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
-                followed: true,
-                firstName: 'Natalia',
-                lastName: 'Kulikova',
-                status: 'I am learning css',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
-                followed: false,
-                firstName: 'Maxim',
-                lastName: 'Bely',
-                status: 'I am learning react',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
-                followed: true,
-                firstName: 'Aleksey',
-                lastName: 'Bely',
-                status: 'I am study at school',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
-                followed: false,
-                firstName: 'Lena',
-                lastName: 'Kulikova',
-                status: 'I go to kindergarten',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-        ]
-    );
-}
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            debugger
+            props.setUsers(response.data.items);
+        })
+    }
+
+
+// props.setUsers(
+//     [
+//         {
+//             id: 1,
+//             photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
+//             followed: true,
+//             firstName: 'Natalia',
+//             lastName: 'Kulikova',
+//             status: 'I am learning css',
+//             location: {city: 'Minsk', country: 'Belarus'}
+//         },
+//         {
+//             id: 2,
+//             photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
+//             followed: false,
+//             firstName: 'Maxim',
+//             lastName: 'Bely',
+//             status: 'I am learning react',
+//             location: {city: 'Minsk', country: 'Belarus'}
+//         },
+//         {
+//             id: 3,
+//             photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
+//             followed: true,
+//             firstName: 'Aleksey',
+//             lastName: 'Bely',
+//             status: 'I am study at school',
+//             location: {city: 'Minsk', country: 'Belarus'}
+//         },
+//         {
+//             id: 4,
+//             photoUrl: 'https://sun9-43.userapi.com/c845322/v845322042/1d433f/CnsL-8jEUjw.jpg',
+//             followed: false,
+//             firstName: 'Lena',
+//             lastName: 'Kulikova',
+//             status: 'I go to kindergarten',
+//             location: {city: 'Minsk', country: 'Belarus'}
+//         },
+//     ]
+
     return (
         <div className={style.usersWrapper}>
             {
-                props.users.map( user => <div key={user.id}>
+                props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt='photo' className={style.userPhoto}/>
+                            <img src={user.photos.small != null ? user.photos.small: userPhoto } alt='photo' className={style.userPhoto}/>
                         </div>
                         <div>
-                            { user.followed
-                                ? <button onClick={ () => {props.unFollow(user.id)}}>Unfollow</button>
-                                : <button onClick={ () => {props.follow(user.id)}}>Follow</button>}
+                            {user.followed
+                                ? <button onClick={() => {
+                                    props.unFollow(user.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(user.id)
+                                }}>Follow</button>}
 
                         </div>
                     </span>
                     <span>
                         <span>
                             <div>
-                                <span>{user.firstName}</span>
-                                <span>{user.lastName}</span>
+                                <span>{user.name}</span>
+
                             </div>
                             <div>
                                 {user.status}
@@ -71,10 +84,10 @@ if (props.users.length === 0) {
                         </span>
                         <span>
                             <div>
-                                {user.location.country}
+                                {'user.location.country'}
                             </div>
                             <div>
-                            {user.location.city}
+                            {'user.location.city'}
                            </div>
                         </span>
                     </span>
@@ -83,5 +96,6 @@ if (props.users.length === 0) {
         </div>
     )
 };
+
 
 export default Users;

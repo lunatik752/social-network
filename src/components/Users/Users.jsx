3c4afb3329b01fab8832c449cd475b9ee065bb36
lunatik752 <whitemaxim752@gmail.com>
2020-06-07 23:21:15 +0300

    added reselect   (lesson 83)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -35,37 +35,29 @@ const Users = (props) => {
                             </NavLink>
                         </div>
                         <div>
-                            {user.followed ? <button disabled={props.followingInProgress.some(id => id === user.id)}
-                                                     onClick={() => {
-                                                         props.unFollow(user.id)
-                                                     }
-                                                     }>Unfollow</button>
+                            {user.followed
+                                ? <button disabled={props.followingInProgress.some(id => id === user.id)}
+                                          onClick={() => {
+                                              props.unFollow(user.id)
+                                          }}>Unfollow
+                                </button>
                                 : <button disabled={props.followingInProgress.some(id => id === user.id)}
                                           onClick={() => {
                                               props.follow(user.id)
-                                          }}
-                                >Follow</button>}
-                                </div>
-                                </span>
-                <span>
-                                <span>
-                                <div>
+                                          }}>Follow
+                                </button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>
                                 <span>{user.name}</span>
-
-                                </div>
-                                <div>
+                            </div>
+                            <div>
                                 {user.status}
-                                </div>
-                                </span>
-                                <span>
-                                <div>
-                                {'user.location.country'}
-                                </div>
-                                <div>
-                                {'user.location.city'}
-                                </div>
-                                </span>
-                                </span>
+                            </div>
+                        </span>
+                    </span>
             </div>)
             }
         </div>

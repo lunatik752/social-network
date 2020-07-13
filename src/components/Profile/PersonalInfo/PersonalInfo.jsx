fb076ac97847cbea1bcc59c11e6ca4a380935a82
lunatik752 <whitemaxim752@gmail.com>
2020-07-13 23:21:34 +0300

    added ProfileData (lesson 97)

--- src/components/Profile/PersonalInfo/PersonalInfo.jsx
+++ src/components/Profile/PersonalInfo/PersonalInfo.jsx
@@ -5,36 +5,14 @@ import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 const PersonalInfo = ({profile, updateStatus, status}) => {
     return (
         <div className={styles.personalInfo}>
-
-            <div>
-                <p>Name: <span>{profile.fullName}</span></p>
-            </div>
-
-            <div>
-                <b>About me:</b> {profile.aboutMe}
-            </div>
-
-            <div>
-                <b>Looking for a job:</b> {profile.lookingForAJob ?  'yes' : 'no'}
-            </div>
-
-            {profile.lookingForAJob && <div>
-                <b>My professional skills:</b> {profile.lookingForAJobDescription}
-            </div>}
-
-            <div>
-                <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
-                  return  <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
-            })}
-            </div>
-
-
-
+            <ProfileData profile={profile}/>
             <ProfileStatusWithHooks updateStatus={updateStatus} status={status}/>
 
         </div>
     )
 };
+
+
 
 
 
@@ -44,6 +22,34 @@ return (
         <b>{contactTitle}: </b>{contactValue}
     </div>
 )
+}
+
+const ProfileData = ({profile}) => {
+    return (
+        <div className={styles.profileData}>
+            <div>
+                <p>Name: <span>{profile.fullName}</span></p>
+            </div>
+
+            <div>
+                <b>About me:</b> {profile.aboutMe}
+            </div>
+
+            <div>
+                <b>Looking for a job:</b> {profile.lookingForAJob ? 'yes' : 'no'}
+            </div>
+
+            {profile.lookingForAJob && <div>
+                <b>My professional skills:</b> {profile.lookingForAJobDescription}
+            </div>}
+
+            <div>
+                <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
+                return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+            })}
+            </div>
+        </div>
+    )
 }
 
 export default PersonalInfo;

093968132d6a01d116e6d875df406a7276cf5bf9
lunatik752 <whitemaxim752@gmail.com>
2020-12-24 14:49:05 +0300

    added antd 2.0/17

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import Main from "./Main";
 import store from "./redux/store";
+import 'antd/dist/antd.css';
 
 
 const App: React.FC = () => {
--- src/Main.tsx
+++ src/Main.tsx
@@ -13,11 +13,17 @@ import Loading from "./common/Loading/Loading";
 import {withSuspense} from "./hoc/withSuspense";
 import {AppRootStateType} from "./redux/store";
 import {UsersPage} from "./components/Users/UsersPage";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const PhotosContainer = React.lazy(() => import('./components/Photos/PhotosContainer'));
 const News = React.lazy(() => import('./components/News/News'));
 const Music = React.lazy(() => import('./components/Music/Music'));
+
 
 
 type MapStateToPropsType = ReturnType<typeof mapStateToProps>
@@ -58,7 +64,78 @@ class Main extends React.Component<MapStateToPropsType & MapDispatchToPropsType>
         }
 
         return (
-            <div className='app-wrapper'>
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout>
+                    <Sider width={200} className="site-layout-background">
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
+                        >
+                            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                                <Menu.Item key="1">option1</Menu.Item>
+                                <Menu.Item key="2">option2</Menu.Item>
+                                <Menu.Item key="3">option3</Menu.Item>
+                                <Menu.Item key="4">option4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                                <Menu.Item key="5">option5</Menu.Item>
+                                <Menu.Item key="6">option6</Menu.Item>
+                                <Menu.Item key="7">option7</Menu.Item>
+                                <Menu.Item key="8">option8</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                                <Menu.Item key="9">option9</Menu.Item>
+                                <Menu.Item key="10">option10</Menu.Item>
+                                <Menu.Item key="11">option11</Menu.Item>
+                                <Menu.Item key="12">option12</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Content
+                            className="site-layout-background"
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                            }}
+                        >
+                            <Switch>
+                                <Route exact path={'/'} render={() => <Redirect to={'/profile/:userId?'}/>}/>
+                                <Route
+                                    path='/profile/:userId?'    // Вопросительный знак в пути означает что параметр опциональный. Его может не быть.
+                                    render={() => <ProfileContainer/>}/>
+                                <Route path='/dialogs'
+                                       render={() => <SuspendedDialogs/>}/>
+                                <Route path='/photos'
+                                       render={() => <SuspendedPhotos/>}/>
+                                <Route path='/news' render={withSuspense(News)}/>
+                                <Route path='/music' render={withSuspense(Music)}/>
+                                <Route path='/settings' render={() => <Settings/>}/>
+                                <Route path='/users' render={() => <UsersPage/>}/>
+                                <Route path='/login' render={() => <Login/>}/>
+                                <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+                            </Switch>
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
+          /*  <div className='app-wrapper'>
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
@@ -79,7 +156,7 @@ class Main extends React.Component<MapStateToPropsType & MapDispatchToPropsType>
                         <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
                     </Switch>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }

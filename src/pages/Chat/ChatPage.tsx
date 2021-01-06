6384e99131bfa205e0407973f50f4647e45954ab
lunatik752 <whitemaxim752@gmail.com>
2021-01-06 21:25:20 +0300

    added receiving and sending messages 2.0/18

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from './Messages.module.css'
+import {useSelector} from "react-redux";
+import {message} from "antd";
+
+const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
+export type ChatMessageType = {
+    message: string
+    photo: string
+    userId: number
+    userName: string
+}
 
 const ChatPage: React.FC = () => {
+
     return (
         <div>
             <Chat/>
@@ -13,6 +25,8 @@ export default ChatPage
 
 
 const Chat: React.FC = () => {
+
+
     return (
         <div>
             <Messages/>
@@ -22,29 +36,31 @@ const Chat: React.FC = () => {
 }
 
 const Messages: React.FC = () => {
-    const messages = [1, 2, 3, 4]
+
+    const [messages, setMessages] = useState<ChatMessageType[]>([])
+
+    useEffect(() => {
+        ws.addEventListener('message', (e) => {
+            const newMessages = JSON.parse(e.data);
+            setMessages((prevMessage) => [...prevMessage, ...newMessages])
+        })
+    }, [])
+
     return (
         <div className={style.messagesWrapper}>
-            {messages.map((m: any) => <Message/>)}
-            {messages.map((m: any) => <Message/>)}
-            {messages.map((m: any) => <Message/>)}
+            {messages.map((m, index) => <Message key={index} message={m}/>)}
+
         </div>
     )
 }
 
-const Message: React.FC = () => {
-    const message = {
-        url: 'https://via.placeholder.com/30',
-        author: 'Max',
-        text: 'Hello'
-
-    }
+const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
 
     return (
         <div>
-            <img src={message.url} alt='photo'/> <b>{message.author}</b>
+            <img src={message.photo} alt='photo'/> <b>{message.userName}</b>
             <br/>
-            {message.text}
+            {message.message}
             <hr/>
         </div>
 
@@ -52,13 +68,26 @@ const Message: React.FC = () => {
 }
 
 const AddMessageChatForm: React.FC = () => {
+
+    const[textMessage, setTextMessage] = useState('')
+
+    const sendMessage = () => {
+        if (!textMessage) {
+            return
+        }
+        ws.send(textMessage)
+        setTextMessage('')
+    }
+
     return (
         <div>
             <div>
-                <textarea></textarea>
+                <textarea
+                    onChange={(e) => setTextMessage(e.currentTarget.value)}
+                    value={textMessage}/>
             </div>
             <div>
-                <button>send</button>
+                <button onClick={sendMessage}>send</button>
             </div>
         </div>
     )

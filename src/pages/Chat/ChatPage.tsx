a3546655e6058a6c26f7cf7f333c1d540408c723
lunatik752 <whitemaxim752@gmail.com>
2021-01-06 14:49:36 +0300

    added components for Chat 2.0/18

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,11 +1,65 @@
 import React from "react";
+import style from './Messages.module.css'
 
 const ChatPage: React.FC = () => {
     return (
         <div>
-            Chat
+            <Chat/>
         </div>
     )
 }
 
 export default ChatPage
+
+
+const Chat: React.FC = () => {
+    return (
+        <div>
+            <Messages/>
+            <AddMessageChatForm/>
+        </div>
+    )
+}
+
+const Messages: React.FC = () => {
+    const messages = [1, 2, 3, 4]
+    return (
+        <div className={style.messagesWrapper}>
+            {messages.map((m: any) => <Message/>)}
+            {messages.map((m: any) => <Message/>)}
+            {messages.map((m: any) => <Message/>)}
+        </div>
+    )
+}
+
+const Message: React.FC = () => {
+    const message = {
+        url: 'https://via.placeholder.com/30',
+        author: 'Max',
+        text: 'Hello'
+
+    }
+
+    return (
+        <div>
+            <img src={message.url} alt='photo'/> <b>{message.author}</b>
+            <br/>
+            {message.text}
+            <hr/>
+        </div>
+
+    )
+}
+
+const AddMessageChatForm: React.FC = () => {
+    return (
+        <div>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>send</button>
+            </div>
+        </div>
+    )
+}
